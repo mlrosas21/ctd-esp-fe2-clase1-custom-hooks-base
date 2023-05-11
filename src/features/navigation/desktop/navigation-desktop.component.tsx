9aba5878f114e6b63c91bb818e4ee2da9208764f
Martin Rosas <mlrosas21@hotmail.com>
2023-05-10 22:13:07 -0300

    useLanguage impl

--- src/features/navigation/desktop/navigation-desktop.component.tsx
+++ src/features/navigation/desktop/navigation-desktop.component.tsx
@@ -1,25 +1,24 @@
 import { SearchBar } from 'features/search';
+import { useLanguage } from 'hooks/useLanguage';
 import { FC } from 'react';
 import { NavLink } from 'react-router-dom';
 
-type NavMobileProps = {
-    t: (key: string) => string
-}
+const NavDesktop: FC = () => {
+  const { translate } = useLanguage();
 
-const NavDesktop: FC<NavMobileProps> = ({t}:NavMobileProps) => {
   return (
     <div className={'container nav'} style={{ justifyContent: 'space-between' }}>
-              <div className={'container'} style={{ width: 400 }}>
-                <NavLink to="/" className={'nav-link'}>
-                  <img className={'logo'} src={'/images/logo-dh.png'} />
-                </NavLink>
+      <div className={'container'} style={{ width: 400 }}>
         <NavLink to="/" className={'nav-link'}>
-          <h3>{t('navigation.home')}</h3>
+          <img className={'logo'} src={'/images/logo-dh.png'} />
         </NavLink>
-              <NavLink to="/following" className={'nav-link'}>
-                <h3>{t('navigation.following')}</h3>
-              </NavLink>
-            </div>
+        <NavLink to="/" className={'nav-link'}>
+          <h3>{translate('navigation.home')}</h3>
+        </NavLink>
+        <NavLink to="/following" className={'nav-link'}>
+          <h3>{translate('navigation.following')}</h3>
+        </NavLink>
+      </div>
       <SearchBar />
     </div>
   );
