7f4e22ba8e1629c68f9366032f9a563a79882f0b
Martin Rosas <mlrosas21@hotmail.com>
2023-05-24 22:54:16 -0300

    route aliases

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig(() => {
-  return {
-    build: {
-      outDir: 'build',
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      src: "/src",
+      hooks: "/src/hooks",
+      features: "/src/features",
+      store: "/src/store"
     },
-    plugins: [react()],
-  };
+  },
 });
